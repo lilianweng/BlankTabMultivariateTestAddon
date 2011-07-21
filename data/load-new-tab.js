5bae203b58716aec28368c8baec62c2ef2927592
Lilian Weng <lilian.wengweng@gmail.com>
2011-07-20 18:24:06 -0700

    add bookmark and recently closed tabs; pin & remove function

--- data/load-new-tab.js
+++ data/load-new-tab.js
@@ -1,7 +1,7 @@
 document.body.innerHTML = 
   '<html>' +
-  '<head><title>New Tab</title><link rel="stylesheet" type="text/css" href="awesometab.css" /></head>' +
-  '<body><h1 id="newtab-title"></h1>' +
+  '<head><title>New Tab</title><link rel="stylesheet" type="text/css" href="tab.css" /></head>' +
+  '<body><div id="header"><h1 id="newtab-title"></h1><a id="clean-blocked" class="clean" href="#">Clean All blocked sites</a> | <a id="clean-star" class="clean" href="#">Clean All starred sites</a></div>' +
   '<div id="content" class="result-wrapper"></div>' +
   '<div id="no-results" style="display:none;">No results</div></body>' +
   '</html>';
