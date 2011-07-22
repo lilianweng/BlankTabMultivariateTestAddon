4ab03a6e949dad950db63c5fc0d72feee87a7d54
Lilian Weng <lilian.wengweng@gmail.com>
2011-07-22 14:50:43 -0700

    refinement

--- data/load-new-tab.js
+++ data/load-new-tab.js
@@ -1,9 +1,14 @@
-document.body.innerHTML = 
-  '<html>' +
-  '<head><title>New Tab</title><link rel="stylesheet" type="text/css" href="tab.css" /></head>' +
-  '<body><div id="header"><h1 id="newtab-title"></h1><a id="clean-blocked" class="clean" href="#">Clean All blocked sites</a> | <a id="clean-star" class="clean" href="#">Clean All starred sites</a></div>' +
+document.documentElement.innerHTML = 
+  '<head>' + 
+  '<meta http-equiv="content-type" content="text/html; charset=UTF-8">' +
+  '<title>New Tab</title>' +
+  '<link rel="stylesheet" type="text/css" href="tab.css" />' +
+  '</head>' +
+  '<body>' +
+  '<div id="header">' +
+  '<h1 id="newtab-title"><a href="testpilot.mozillalabs.com" target="_blank">Test Pilot</a> New Tab Test</h1>' +
+  '<a id="clean-blocked" class="clean" href="#">Clean All blocked sites</a> | <a id="clean-star" class="clean" href="#">Clean All starred sites</a>' +
+  '</div>' +
   '<div id="content" class="result-wrapper"></div>' +
-  '<div id="no-results" style="display:none;">No results</div></body>' +
-  '</html>';
-
-$("#newtab-title").html("TestPilot New Tab Test");
+  '<div id="no-results" style="display:none;">No results</div>';
+  '</body>';
