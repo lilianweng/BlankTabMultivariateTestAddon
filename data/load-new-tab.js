321acb4d343e1a0df13730d50063f5551899b065
Lilian Weng <lilian.wengweng@gmail.com>
2011-07-26 14:38:50 -0700

    removing blocked/pinned sites in fallbacks

--- data/load-new-tab.js
+++ data/load-new-tab.js
@@ -5,11 +5,13 @@ document.documentElement.innerHTML =
   '<link rel="stylesheet" type="text/css" href="tab.css" />' +
   '</head>' +
   '<body>' +
+  '<div id="page-wrap-newblanktabtestaddonthing">' + 
   '<div id="header">' +
   '<h1 id="newtab-title"><a href="http://testpilot.mozillalabs.com/" target="_blank">Test Pilot</a> New Tab Test</h1>' +
   '<a id="clean-blocked" class="clean" href="#">Clean All blocked sites</a>' + 
   //' | <a id="clean-star" class="clean" href="#">Clean All starred sites</a>' +
   '</div>' +
   '<div id="content" class="result-wrapper"></div>' +
-  '<div id="no-results" style="display:none;">No results</div>';
-  '</body>';
+  '<div id="no-results" style="display:none;">No results</div>' + 
+  '</div>' + 
+   '</body>';
